cb7e1a25e017992bfca0c96bc4ba1a1c3f6f6324
krish <krishnaja-b>
2023-05-03 23:43:14 +0530

    usuage of star means for all instances

--- roboshop-setup/ex2/main.tf
+++ roboshop-setup/ex2/main.tf
@@ -1,4 +1,4 @@
-data "aws_ami" "ami" {
+ data "aws_ami" "ami" {
   most_recent      = true
   name_regex       = "Centos-8-DevOps-Practice"
   owners           = ["902151144678"]
@@ -15,3 +15,7 @@ resource "aws_instance" "frontend" {
 variable "instances" {
   default = ["cart","catalouge","user","payment","shipping"]
 }
+
+ output "public-ip" {
+   value = aws_instance.frontend.*.public_ip
+ }
