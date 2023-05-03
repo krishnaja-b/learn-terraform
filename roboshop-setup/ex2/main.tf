2b4c308b3f61f24de191ded89392522aa5f24a91
krish <krishnaja-b>
2023-05-04 00:51:47 +0530

    to get instance names

--- roboshop-setup/ex2/main.tf
+++ roboshop-setup/ex2/main.tf
@@ -9,6 +9,9 @@ resource "aws_instance" "frontend" {
   ami = data.aws_ami.ami.image_id
   instance_type = "t3.micro"
   vpc_security_group_ids = ["sg-003f987727c69b5e1"]
+  tags = {
+    Name=var.instances[count.index]
+  }
 
 }
 
