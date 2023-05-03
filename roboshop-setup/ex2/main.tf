6618e6c9dfa7932fc26e81361c5f20912548206e
krish <krishnaja-b>
2023-05-03 23:19:10 +0530

    usuage of length function

--- roboshop-setup/ex2/main.tf
+++ roboshop-setup/ex2/main.tf
@@ -1,13 +1,17 @@
 data "aws_ami" "ami" {
   most_recent      = true
-  name_regex       = "0b5a2b5b8f2be4ec2"
+  name_regex       = "Centos-8-DevOps-Practice"
   owners           = ["902151144678"]
 }
 
 resource "aws_instance" "frontend" {
-  count=5
+  count=length(var.instances)
   ami = data.aws_ami.ami.image_id
   instance_type = "t3.micro"
   vpc_security_group_ids = ["sg-003f987727c69b5e1"]
 
+}
+
+variable "instances" {
+  default = ["cart","catalouge","user","payment","shipping"]
 }
