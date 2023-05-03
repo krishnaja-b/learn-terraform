640f928ee7c19768ad37888cf28e5f8a1359a522
krish <krishnaja-b>
2023-05-03 22:10:39 +0530

    s3 backend

---
+++ s3-state/main.tf
@@ -0,0 +1,17 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-aws43"
+    key    = "s3-state/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+
+resource "aws_instance" "ec2" {
+  ami = "ami-0b5a2b5b8f2be4ec2"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = ["sg-003f987727c69b5e1"]
+  tags = {
+    Name="test"
+  }
+}
