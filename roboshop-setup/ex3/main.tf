bf3799fe2b648ea8a57a70cd2dc85c9785f55127
krish <krishnaja-b>
2023-05-04 23:52:43 +0530

    using for_each

---
+++ roboshop-setup/ex3/main.tf
@@ -0,0 +1,28 @@
+data "aws_ami" "ami" {
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-Practice"
+  owners           = ["973714476881"]
+}
+
+resource "aws_instance" "instances" {
+  for_each = var.instances
+  ami = "ami-0b5a2b5b8f2be4ec2"
+  instance_type = each.value["name"]
+  vpc_security_group_ids = ["sg-003f987727c69b5e1"]
+  tags = {
+    Name=each.value["name"]
+  }
+}
+
+variable "instances" {
+  default = {
+    catalouge= {
+        name="catalouge"
+         type="t3.micro"
+    }
+    user={
+        name="user"
+        type="t3.small"
+    }
+  }
+  }
