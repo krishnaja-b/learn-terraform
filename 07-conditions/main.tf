e5b716a32716dd663f25d2607be9a8a39b5bb8df
krish <krishnaja-b>
2023-05-05 20:05:19 +0530

    using count in  condition syntax

--- 07-conditions/main.tf
+++ 07-conditions/main.tf
@@ -7,7 +7,7 @@ variable "instance_type" {}
 
 variable "create_instances" {}
 
-resource "aws_instance" "test" {
+resource "aws_instance" "test1" {
   count= var.create_instances == "true" ? 1 : 0
   ami = "ami-0b5a2b5b8f2be4ec2"
   instance_type = var.instance_type == ""  ? "t3.micro" : var.instance_type
