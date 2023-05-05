6623e781f2209bd53b8d234327c4a256f1189cd9
krish <krishnaja-b>
2023-05-05 19:52:32 +0530

    using condition syntax

--- 07-conditions/main.tf
+++ 07-conditions/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "test" {
   ami = "ami-0b5a2b5b8f2be4ec2"
-  instance_type = var.instance_type
+  instance_type = var.instance_type == ""  ? "t3.micro" : var.instance_type
 }
 
 variable "instance_type" {}
