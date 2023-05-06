37ec281acaec7bb6508e3cc888f3d5ae5433dcdd
krish <krishnaja-b>
2023-05-07 00:21:29 +0530

    ex5,for sharing data between modules

--- roboshop-setup/ex5/route53/main.tf
+++ roboshop-setup/ex5/route53/main.tf
@@ -1,7 +1,7 @@
 resource "aws_route53_record" "record" {
   zone_id = "Z013366722COOZOYCJ4IN"
-  name    = "${var.component}-dev.aws43.xyz"
+  name    = "${"var.component"}-dev.aws43.xyz"
   type    = "A"
   ttl     = 30
-  records = [aws_instance.ec2.private_ip]
+  records = [aws_instance_ec2_private_ip]
 }
